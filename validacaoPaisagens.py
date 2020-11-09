0a32de6187e8442c7759c6858958969e236a1056
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2020-11-09 20:28:13 -0300

    Add files via upload

--- validacaoPaisagens.py
+++ validacaoPaisagens.py
@@ -1,4 +1,4 @@
-## Para chamar este script: exec(open('C:/Paisagens/recebimentoPaisagem/validacaoPaisagens.py'.encode('utf-8')).read())
+## Para chamar este script: exec(open('C:/FUSION/TAN_A01/validacaoPaisagens.py'.encode('utf-8')).read())
 
 import subprocess
 import os
@@ -6,18 +6,18 @@ import os
 FUSION_FOLDER = 'C:/FUSION'
 LASTOOLS_FOLDER = 'C:/LAStools/bin'
 
-NP_FOLDER = "C:/Paisagens/recebimentoPaisagem/NP"
-NP_DTM_FOLDER = "C:/Paisagens/recebimentoPaisagem/NP_DTM"
-DTM_FOLDER = "C:/Paisagens/recebimentoPaisagem/DTM"
-DTM_PREVIOUS = "C:/Paisagens/recebimentoPaisagem/DTM_ANTERIOR"
+NP_FOLDER = "C:/FUSION/TAN_A01/NP"
+NP_DTM_FOLDER = "C:/FUSION/TAN_A01/NP_MDT"
+DTM_FOLDER = "C:/FUSION/TAN_A01/MDT"
+DTM_PREVIOUS = "C:/FUSION/TAN_A01/MDT_ANTERIOR"
 
-VALIDA_FOLDER = 'C:/Paisagens/recebimentoPaisagem'
+VALIDA_FOLDER = 'C:/FUSION/TAN_A01/check'
 PROJETO = 'TAN_A01'
 crs = QgsCoordinateReferenceSystem("EPSG:31982")
 
 ## Cria pasta para projeto 
 try:
-   os.mkdir(VALIDA_FOLDER + '/' + PROJETO)
+   os.mkdir(VALIDA_FOLDER)
 except OSError:
    print ("Creation of the directory failed.")
 else:
@@ -28,13 +28,13 @@ else:
 try:
    subprocess.call(FUSION_FOLDER + '/Catalog /image /drawtiles /countreturns /density:1,4,8 ' +
                    NP_DTM_FOLDER + '/' + PROJETO + '*.las ' +
-                   VALIDA_FOLDER + '/' + PROJETO + '/' + PROJETO +'catalog')
+                   VALIDA_FOLDER + '/' + PROJETO +'catalog')
 except OSError:
    print('Return density for %s was not computed.' % PROJETO)
 else:
    print('Return density for %s was successfully computed!' % PROJETO)
 
-density = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + PROJETO + 'catalog_return_density.jpg', 'returnsDensity')
+density = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + 'catalog_return_density.jpg', 'returnsDensity')
 density.setCrs(crs)
 QgsProject.instance().addMapLayer(density)
 
@@ -42,7 +42,7 @@ QgsProject.instance().addMapLayer(density)
 ## Roda LASTOOLS::lasinfo
 try:
    subprocess.call(LASTOOLS_FOLDER+'/lasinfo -cpu64 -i '+ NP_FOLDER + '/' + PROJETO + '*.las -merged -odir '+ 
-                   VALIDA_FOLDER + '/' + PROJETO + ' -o "report.txt" -cd -histo gps_time 20')
+                   VALIDA_FOLDER + ' -o "report.txt" -cd -histo gps_time 20')
 except OSError:
    print('Information for %s was not extracted.' % PROJETO)
 else:
@@ -52,36 +52,52 @@ else:
 ## Computar densidade: ReturnDensity [switches] outputfile cellsize datafile1
 try:
     subprocess.call(FUSION_FOLDER + '/ReturnDensity /ascii ' +
-                    VALIDA_FOLDER + '/' + PROJETO + '/' + 'density.asc 5 ' +
+                    VALIDA_FOLDER + '/' + 'density.asc 5 ' +
                     NP_FOLDER + '/' + PROJETO + '*.las')
 except OSError:
    print('Return density for %s was not computed.' % PROJETO)
 else:
    print('Return density for %s was successfully computed!' % PROJETO)
 
-densidade = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + 'density.asc', 'densidade')
+densidade = QgsRasterLayer(VALIDA_FOLDER + '/' + 'density.asc', 'densidade')
 densidade.setcrs(crs)
 QgsProject.instance().addMapLayer(densidade)   
 
 ## Unir DTM entregue
+try:
+   mdts = []
+   for filename in os.listdir(DTM_FOLDER):
+        if file.endswith(".flt"):
+            mdts.append(os.path.join(DTM_FOLDER+'/'+filename))
+   processing.run("gdal:merge", {'INPUT':mdts,
+                                 'PCT':False,
+                                 'SEPARATE':False,
+                                 'NODATA_INPUT':None,
+                                 'NODATA_OUTPUT':-9999, #Prevous None
+                                 'OPTIONS':'',
+                                 'EXTRA':'',
+                                 'DATA_TYPE':5,
+                                 'OUTPUT':VALIDA_FOLDER + '/' + 'mdtEntregue.tif'})
+except OSError:
+   print('Previous DTM from %s was not merged.' % PROJETO)
+else:
+   print('Previous DTM from %s was successfully merged!' % PROJETO)
 
-
-#Code...
 
 
 ## Computar hillshade
 try:
-    processing.run("native:hillshade", {'INPUT':VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtEntregue.tif',
+    processing.run("native:hillshade", {'INPUT':VALIDA_FOLDER + '/' + 'mdtEntregue.tif',
                                         'Z_FACTOR':3,
                                         'AZIMUTH':300,
                                         'V_ANGLE':40,
-                                        'OUTPUT':VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtEntregue.tif'})
+                                        'OUTPUT':VALIDA_FOLDER + '/' + 'mdtEntregue.tif'})
 except OSError:
    print('Hillshade for %s not created.' % PROJETO)
 else:
    print('Hillshade for %s successfully computed!' % PROJETO)
    
-mdtHillshade = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtHillshade.tif', 'mdtHillshade')
+mdtHillshade = QgsRasterLayer(VALIDA_FOLDER + '/' + 'mdtHillshade.tif', 'mdtHillshade')
 mdtHillshade.setcrs(crs)
 QgsProject.instance().addMapLayer(mdtHillshade)
 
@@ -89,12 +105,12 @@ QgsProject.instance().addMapLayer(mdtHillshade)
 ## Gera o modelo digital de terreno a partir da nuvem entregue
 try:
    subprocess.call(FUSION_FOLDER + '/GridSurfaceCreate ' +
-                   VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.dtm ' +
+                   VALIDA_FOLDER + '/' + 'mdtCriado.dtm ' +
                    '1 m m 0 0 0 0 ' + NP_DTM_FOLDER + '/' + PROJETO + '*.las')
                    
    subprocess.call(FUSION_FOLDER + '/DTM2ASCII ' +
-                   VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.dtm ' +
-                   VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.asc')
+                   VALIDA_FOLDER + '/' + 'mdtCriado.dtm ' +
+                   VALIDA_FOLDER + '/' + 'mdtCriado.asc')
 except OSError:
    print('DTM for %s was not created.' % PROJETO)
 else:
@@ -114,37 +130,57 @@ try:
                                  'OPTIONS':'',
                                  'EXTRA':'',
                                  'DATA_TYPE':5,
-                                 'OUTPUT':VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtAnterior.tif'})
+                                 'OUTPUT':VALIDA_FOLDER + '/' + 'mdtAnterior.tif'})
 except OSError:
    print('Previous DTM from %s was not merged.' % PROJETO)
 else:
    print('Previous DTM from %s was successfully merged!' % PROJETO)
 
-mdtAnterior = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtAnterior.tif', "mdtAnterior")
-mdtCriado = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.asc', "mdtCriado")
+mdtEntregue = QgsRasterLayer(VALIDA_FOLDER + '/' + 'mdtEntregue.tif', "mdtEntregue")
+mdtAnterior = QgsRasterLayer(VALIDA_FOLDER + '/' + 'mdtAnterior.tif', "mdtAnterior")
+mdtCriado = QgsRasterLayer(VALIDA_FOLDER + '/' + 'mdtCriado.asc', "mdtCriado")
+mdtEntregue.setCrs(crs)
 mdtAnterior.setCrs(crs)
 mdtCriado.setCrs(crs)
+QgsProject.instance().addMapLayer(mdtEntregue)
 QgsProject.instance().addMapLayer(mdtAnterior)
 QgsProject.instance().addMapLayer(mdtCriado)
 
 
 ## Calcula diferença entre MDTs
 try:
-   processing.run("saga:rastercalculator", {'GRIDS':mdtCriado,
+   processing.run("saga:rastercalculator", {'GRIDS':mdtEntregue,
                                             'XGRIDS': mdtAnterior, 
                                             'FORMULA':'a-b',
                                             'RESAMPLING':3,
                                             'USE_NODATA':False,
                                             'TYPE':7,
-                                            'RESULT': VALIDA_FOLDER + '/' + PROJETO + '/' + 'diffCriadoAnterior.tif'})
+                                            'RESULT': VALIDA_FOLDER + '/' + 'diffCriadoAnterior.tif'})
 except OSError:
    print('Diff between DTMs for %s was not computed.' % PROJETO)
 else:
    print('Diff between DTMs for %s successfully computed!' % PROJETO)
     
-diffAntCriado = QgsRasterLayer(VALIDA_FOLDER + '/' + PROJETO + '/' + 'diffCriadoAnterior.sdat', 'diffCriadoAnt')
-diffAntCriado.setCrs(crs)
-QgsProject.instance().addMapLayer(diffAntCriado)
+diffEntregueCriado = QgsRasterLayer(VALIDA_FOLDER + '/' + 'diffEntregueAnterior.sdat', 'diffEntregueAnt')
+diffEntregueCriado.setCrs(crs)
+QgsProject.instance().addMapLayer(diffEntregueCriado)
+
+try:
+   processing.run("saga:rastercalculator", {'GRIDS':mdtCriado,
+                                            'XGRIDS': mdtEntregue, 
+                                            'FORMULA':'a-b',
+                                            'RESAMPLING':3,
+                                            'USE_NODATA':False,
+                                            'TYPE':7,
+                                            'RESULT': VALIDA_FOLDER + '/' + 'diffCriadoEntregue.tif'})
+except OSError:
+   print('Diff between DTMs for %s was not computed.' % PROJETO)
+else:
+   print('Diff between DTMs for %s successfully computed!' % PROJETO)
+    
+diffCriadoEntregue = QgsRasterLayer(VALIDA_FOLDER + '/' + 'diffCriadoEntregue.sdat', 'diffCriadoEntregue')
+diffCriadoEntregue.setCrs(crs)
+QgsProject.instance().addMapLayer(diffCriadoEntregue)
 
 
 ## Extrair Hmean
@@ -152,8 +188,8 @@ try:
     for filename in os.listdir(NP_FOLDER):
            os.path.join(NP_FOLDER+'/'+filename)
            subprocess.call(FUSION_FOLDER + '/GridMetrics /nointensity /nocsv /raster:mean /ascii ' +
-                           VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.dtm 10 10 ' +
-                           VALIDA_FOLDER + '/' + PROJETO + '/' + os.path.splitext(filename)[0]+'.asc ' +
+                           VALIDA_FOLDER + '/' + 'mdtCriado.dtm 10 10 ' +
+                           VALIDA_FOLDER + '/' + os.path.splitext(filename)[0]+'.asc ' +
                            os.path.join(NP_FOLDER+'/'+filename))
 except OSError:
    print('Hmean for %s was not computed.' % PROJETO)
@@ -166,8 +202,8 @@ try:
     for filename in os.listdir(NP_FOLDER):
            os.path.join(NP_FOLDER+'/'+filename)
            subprocess.call(FUSION_FOLDER + '/GridMetrics /nointensity /nocsv /raster:max /ascii ' +
-                           VALIDA_FOLDER + '/' + PROJETO + '/' + 'mdtCriado.dtm 10 1 ' +
-                           VALIDA_FOLDER + '/' + PROJETO + '/' + os.path.splitext(filename)[0]+'.asc ' +
+                           VALIDA_FOLDER + '/' + 'mdtCriado.dtm 10 1 ' +
+                           VALIDA_FOLDER + '/' + os.path.splitext(filename)[0]+'.asc ' +
                            os.path.join(NP_FOLDER+'/'+filename))
 except OSError:
    print('Hmax for %s was not computed.' % PROJETO)
