664d7faa34f01e5403d91658c06e24a6fb162010
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-11-13 23:38:23 -0300

    Create clip_las.bat

---
+++ fusion/clip_las.bat
@@ -0,0 +1,7 @@
+REM X: 400134
+REM Y: 13990
+REM ClipData [switches] InputSpecifier SampleFile [MinX MinY MaxX MaxY]
+
+c:\fusion\clipdata C:\FUSION\eba\NP_T-0477.laz C:\FUSION\eba\tree0477.laz 400034 13890 400234 14090
+
+pause
